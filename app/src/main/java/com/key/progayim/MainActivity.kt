18a9bbe5a85d3dc0c21fdac668c4b3e45efa6212
zhanyll <janylayka95@gmail.com>
2021-11-22 17:55:40 +0600

    les

--- app/src/main/java/com/key/progayim/MainActivity.kt
+++ app/src/main/java/com/key/progayim/MainActivity.kt
@@ -3,8 +3,32 @@ package com.key.progayim
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = Adapter {
+            Toast.makeText(this, "ITEM - $it", Toast.LENGTH_SHORT).show()
+        }
+
+        recycler.layoutManager = layoutManager
+        recycler.adapter = adapter
+        recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
+
+        val list = mutableListOf<String>()
+        for (i in 0..20) {
+            list.add("ITEM - $i")
+        }
+        adapter.setData(list)
+    }
 
 }
